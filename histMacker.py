c85ff66551d53c661f9c27bc45244e24f76f1f4f
14littleraindrop <14littleraindrop@gmail.com>
2020-03-14 15:33:12 -0700

    Add json file to configure histMacker.py
    - Add weight (both even + sample weights) to histogram

--- histMacker.py
+++ histMacker.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import numpy as np
 import sys
+import json
 import math
 import matplotlib
 matplotlib.use('pdf')
@@ -20,34 +21,49 @@ parser.add_argument('--bin', type=int, nargs=1, help='Number of bins', required=
 args = parser.parse_args()
 directory = args.dir[0]
 variable = args.var[0]
+with open('histMacker.json') as f:
+    config = json.load(f)
+weight = config['weight']
+trigger = config['trigger']
 
 from ROOT import TFile
 from ROOT.Math import PtEtaPhiEVector
 
-def make_hist():
-    for name in os.listdir(directory):
-        print 'Processing: %s ' % (name)
-        bin_width = (args.range[1] - args. range[0]) / args.bin[0]
-        hist = {}
-        for i in range (0, args.bin[0]):
-            hist[(i + 1) * bin_width] = 0.
-        with open(os.path.join(directory, name), 'r') as f:
-            for line in f.readlines():
-                tfile = TFile(line.strip())
-                mychain = tfile.Get('FlavourTagging_Nominal')
-                entries = mychain.GetEntriesFast()
-                print "Collecting %s vakues: %d entries..." % (variable, entries)
+for name in os.listdir(directory):
+    print 'Processing: %s ' % (name)
+    bin_width = (args.range[1] - args. range[0]) / args.bin[0]
+    hist = {}
+    for i in range (0, args.bin[0]):
+        hist[(i + 1) * bin_width] = 0.
 
-                for i in range(entries):
-                    nb = mychain.GetEntry(i)
-                    if nb <= 0:
-                        continue 
-                    if variable == 'fat_pt':
-                        value = mychian.fat_pt
-                    else:
-                        raise ValueError('Unsupported variable name.')
-                    for item in value:
-                        if not np.isnan(item) and not np.isinf(item):
-                            selected_bin = min([ x - value for x in hist.keys()], key=abs) + value
-                            # TODO: add weighting
-                            hist[selected_bin] = hist[selected_bin] + 1
+    # Get weighting info for the slice
+    sum_of_w = 0
+    xsec = weight[name.split('.')[3]]['xsec']
+    filterEff = weight[name.split('.')[3]]['filterEff']
+
+    # Loop over root files in one mc slice
+    with open(os.path.join(directory, name), 'r') as f:
+        for line in f.readlines():
+            tfile = TFile(line.strip())
+            mychain = tfile.Get('FlavourTagging_Nominal')
+            entries = mychain.GetEntriesFast()
+            print "Collecting %s vakues: %d entries..." % (variable, entries)
+
+            # Get leaf value
+            for i in range(entries):
+                nb = mychain.GetEntry(i)
+                if nb <= 0:
+                    continue 
+                if variable == 'fat_pt':
+                    value = mychain.fat_pt
+                else:
+                    raise ValueError('Unsupported variable name.')
+                mc_eve_w = mychain.eve_mc_w * mychain.eve_pu_w
+                sum_of_w = sum_of_w + mc_eve_w
+                for item in value:
+                    if not np.isnan(item) and not np.isinf(item):
+                        selected_bin = min([ x - item for x in hist.keys()], key=abs) + item
+                        hist[selected_bin] = hist[selected_bin] + (1 * mc_eve_w)
+
+    # Add on slice-wise weight
+    hist.update((x, y * xsec * filterEff / sum_of_w) for x, y in hist.items())
