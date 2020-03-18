1dd319632c0daacd93e2f79af0c84ee38ac155a1
14littleraindrop <14littleraindrop@gmail.com>
2020-03-17 21:18:16 -0700

    Add trigger to select entries to plot, add Histogram class
    - Tweak config values in JSON file

--- histMacker.py
+++ histMacker.py
@@ -8,6 +8,9 @@ import matplotlib
 matplotlib.use('pdf')
 matplotlib.rc('figure', figsize=(8, 5))
 import matplotlib.pyplot as plt
+import cPickle as pickle
+
+from histogram import Histogram
 
 parser = argparse.ArgumentParser(description='Make histogram of a variable given a directory of \
         text files containing ROOT file pathes of all five MC slices.')
@@ -29,51 +32,69 @@ trigger = config['trigger']
 from ROOT import TFile
 from ROOT.Math import PtEtaPhiEVector
 
-hist_all = {}
-bin_width = (args.range[1] - args. range[0]) / args.bin[0]
-for i in range (0, args.bin[0]):
-    hist_all[(i + 1) * bin_width] = 0
-for name in os.listdir(directory):
-    print 'Processing: %s... ' % (name)
-    hist = {}
+###################
+def trigger():
+    # Check fatjet_eta
+    all(trigger['fat_eta'][0] <= x <= trigger['fat_eta'][1] for x in mychain.fat_eta) and \
+    # Check leading_fatjet_pt
+    mychain.fat_pt >= trigger['fat_pt']
+###################
+
+with open(directory+'.pickle', 'wb') as output:
+    hist_all = {}
+    bin_width = (args.range[1] - args. range[0]) / args.bin[0]
     for i in range (0, args.bin[0]):
-        hist[(i + 1) * bin_width] = 0.
+        hist_all[(i + 1) * bin_width] = 0
+    for name in os.listdir(directory):
+        print 'Processing: %s... ' % (name)
+        hist = {}
+        for i in range (0, args.bin[0]):
+            hist[(i + 1) * bin_width] = 0.
 
-    # Get weighting info for the slice
-    sum_of_w = 0
-    xsec = weight[name.split('.')[3]]['xsec']
-    filterEff = weight[name.split('.')[3]]['filterEff']
+        # Get weighting info for the slice
+        sum_of_w = 0
+        xsec = weight[name.split('.')[3]]['xsec']
+        filterEff = weight[name.split('.')[3]]['filterEff']
 
-    # Loop over root files in one mc slice
-    with open(os.path.join(directory, name), 'r') as f:
-        for line in f.readlines():
-            tfile = TFile(line.strip())
-            mychain = tfile.Get('FlavourTagging_Nominal')
-            entries = mychain.GetEntriesFast()
-            print 'Collecting %s values: %d entries...' % (variable, entries)
+        # Loop over root files in one mc slice
+        with open(os.path.join(directory, name), 'r') as f:
+            for line in f.readlines():
+                tfile = TFile(line.strip())
+                mychain = tfile.Get('FlavourTagging_Nominal')
+                entries = mychain.GetEntriesFast()
+                print 'Collecting %s values: %d entries...' % (variable, entries)
 
-            # Get leaf value
-            for i in range(entries):
-                nb = mychain.GetEntry(i)
-                if nb <= 0:
-                    continue 
-                if variable == 'fat_pt':
-                    value = mychain.fat_pt
-                else:
-                    raise ValueError('Unsupported variable name.')
-                mc_eve_w = mychain.eve_mc_w * mychain.eve_pu_w
-                sum_of_w = sum_of_w + mc_eve_w
-                for item in value:
-                    if not np.isnan(item) and not np.isinf(item):
-                        selected_bin = min([ x - item for x in hist.keys()], key=abs) + item
-                        hist[selected_bin] = hist[selected_bin] + (1 * mc_eve_w)
+                # Get leaf value
+                for i in range(entries):
+                    nb = mychain.GetEntry(i)
+                    if nb <= 0:
+                        continue
 
-    # Add on slice-wise weight
-    hist.update((x, y * xsec * filterEff / sum_of_w) for x, y in hist.items())
-    for key in hist.keys():
-        hist_all[key] = hist_all[key] + hist[key]
-    # TODO: Save histogram
-    print 'Histogram for %s has been created.' % (name)
+                    # Chekc trigger value
+                    #################
+                    if mychain.trigger['eve_HLT']['variable'] != 1:
+                        continue
+                    if mychain.trigger['leading_fat_pt']
+                    #################
 
-print 'Combined histogram has been created.'
-print 'Done!'
+                    if variable == 'fat_pt':
+                        value = mychain.fat_pt
+                    else:
+                        raise ValueError('Unsupported variable name.')
+                    mc_eve_w = mychain.eve_mc_w * mychain.eve_pu_w
+                    sum_of_w = sum_of_w + mc_eve_w
+                    for item in value:
+                        if not np.isnan(item) and not np.isinf(item):
+                            selected_bin = min([ x - item for x in hist.keys()], key=abs) + item
+                            hist[selected_bin] = hist[selected_bin] + (1 * mc_eve_w)
+
+        # Add on slice-wise weight
+        hist.update((x, y * xsec * filterEff / sum_of_w) for x, y in hist.items())
+        for key in hist.keys():
+            hist_all[key] = hist_all[key] + hist[key]
+        # TODO: rename hist before dumpping
+        pickle.dump(hist, output, protocol = pickle.HIGHEST_PROTOCOL)
+        print 'Histogram for %s has been created.' % (name)
+    pickle.dump(hist_all, output, protocol = pickle.HIGHEST_PROTOCOL)
+    print 'Combined histogram has been created.'
+    print 'Done!'
---
+++ histogram.py
@@ -0,0 +1,39 @@
+import cPickle as pickle
+import numpy as np
+import os
+
+class Histogram(object):
+    def __init__(self, name):
+        self.name = name
+        self.data = dict()
+
+    def setup_bins(self, minimum, maximum, num_bins):
+        bin_values = np.linspace(minimum, maximum, num_bins)
+        for val in bin_values:
+            self.data[val] = 0.0
+
+    def bins(self):
+        return self.data.keys()
+
+    def frequencies(self):
+        return self.data.values()
+
+    def add_point(self, value, weight=1):
+        if len(self.bins()) == 0:
+            raise ValueError('histogram has no bins')
+        difference = value - np.array(sorted(self.bins()))
+        index = np.where(difference > 0, difference, np.inf).argmin()
+        self.data[sorted(self.bins())[index]] += weight
+
+    def rescale(self, weight):
+        for b in self.bins():
+            self.data[b] *= weight
+
+    def combine(self, other):
+        for b in self.bins():
+            self.data[b] += other.data[b]
+
+    def pickle(self, directory=''):
+        with open(os.path.join(directory, self.name + '.pickle'), 'wb') as f:
+            pickle.dump(self, f, protocol=pickle.HIGHEST_PROTOCOL)
+
